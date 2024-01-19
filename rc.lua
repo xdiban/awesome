fa9b51a4345b70e07577d243ed09e9b2d9da98ad
xavi <xdiba@pm.me>
2024-01-19 04:03:03 +0100

    added picom conf

--- rc.lua
+++ rc.lua
@@ -613,6 +613,6 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 -- }}}
 
 -- Autostart applications:
-awful.spawn.with_shell("picom --experimental-backend --transparent-clipping")
+awful.spawn.with_shell("picom")
 awful.spawn.once("nitrogen --restore")
 awful.spawn.with_shell("setxkbmap -model pc105 -option 'grp:shifts_toggle,compose:sclk' 'us,us(intl)'")
