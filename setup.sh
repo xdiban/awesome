a6bbbf719f0bc3dee40f8903d3dd047d6478fd5e
xavi <xdiba@pm.me>
2024-02-06 20:49:26 +0100

    papirus

--- setup.sh
+++ setup.sh
@@ -7,4 +7,4 @@
 yay -Syu
 
 #Install dependencies
-yay -S volctl picom lxsession redshift-gtk flameshot kitty nvim firefox i3lock xtrlock pcmanfm xfce4-power-manager network-manager-applet xorg-server xorg-xinit materia-gtk-theme capitaine-cursors lxapparence
+yay -S volctl picom lxsession redshift-gtk flameshot kitty nvim firefox i3lock xtrlock pcmanfm xfce4-power-manager network-manager-applet xorg-server xorg-xinit materia-gtk-theme capitaine-cursors lxapparence-gtk3 papirus-icon-theme
