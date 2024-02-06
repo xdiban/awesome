ccde768a7c318628cb010a78513bfa0179bd32e3
xavi <xdiba@pm.me>
2024-02-06 21:06:14 +0100

    bluetooth

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ sudo pacman -Sy archlinux-keyring
 yay -Syu
 
 #Install dependencies
-yay -S volctl picom lxsession redshift flameshot kitty nvim firefox i3lock xtrlock pcmanfm xfce4-power-manager network-manager-applet xorg-server xorg-xinit materia-gtk-theme capitaine-cursors lxapparence-gtk3 papirus-icon-theme
+yay -S volctl picom lxsession redshift flameshot kitty nvim firefox i3lock xtrlock pcmanfm xfce4-power-manager network-manager-applet xorg-server xorg-xinit materia-gtk-theme capitaine-cursors lxapparence-gtk3 papirus-icon-theme blueman
 
 sudo systemctl enable --now geoclue.service
+sudo systemctl enable --now bluetooth.service
