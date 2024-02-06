ba4653399a47ff43d05cbc52675c3c0021fd2f2e
xavi <xdiba@pm.me>
2024-02-06 20:47:04 +0100

    added lxapparence added

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #Update keyring
-sudo pacman -Sy archlinux-keyring
+#sudo pacman -Sy archlinux-keyring
 
 #Update system
 yay -Syu
 
 #Install dependencies
-yay -S volctl picom lxsession redshift-gtk flameshot kitty nvim firefox i3lock xtrlock pcmanfm xfce4-power-manager network-manager-applet xorg-server xorg-xinit materia-gtk-theme capitaine-cursors 
+yay -S volctl picom lxsession redshift-gtk flameshot kitty nvim firefox i3lock xtrlock pcmanfm xfce4-power-manager network-manager-applet xorg-server xorg-xinit materia-gtk-theme capitaine-cursors lxapparence
