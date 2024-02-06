260d40b4f99af45a043a9bffaf79216e9362ff50
xavi <xdiba@pm.me>
2024-02-06 18:58:33 +0100

    added script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Update system
+yay -Syu
+
+#Install dependencies
+yay -S volctl picom lxsession redshift-gtk flameshot kitty nvim firefox i3lock xtrlock pcmanfm xfce4-power-manager network-manager-applet xorg-server xorg-xinit
